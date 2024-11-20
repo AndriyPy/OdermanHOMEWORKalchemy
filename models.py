307f163d07b1bfc068588df6851d70608c72b6e1
Andriyko222 <125126827+Andriyko222@users.noreply.github.com>
2024-11-20 21:46:01 +0200

    alchemy220

---
+++ models.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine, String
+from sqlalchemy.orm import DeclarativeBase, sessionmaker, Mapped, mapped_column
+
+DB = "sqlite:///app.db"
+engine = create_engine(DB, echo=True)
+
+Session = sessionmaker(bind=engine)
+
+class Base(DeclarativeBase):
+    ...
+
+class Event(Base):
+    __tablename__ = "events"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    description: Mapped[str] = mapped_column(String(240), nullable=True)
+    pizzaname: Mapped[str] = mapped_column(String(50))
+
+    def __repr__(self):
+        return f"Pizzaname:{self.pizzaname}, description:{self.description}"
+
+def create_db():
+    Base.metadata.create_all(engine)
+
+def drop_db():
+    Base.metadata.drop_all(engine)
