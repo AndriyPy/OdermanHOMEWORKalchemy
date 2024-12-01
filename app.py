4d053f448c81b8d7cb810160beecca3012c440e5
Andriyko222 <125126827+Andriyko222@users.noreply.github.com>
2024-12-01 19:33:13 +0200

    alchemy5.0

--- app.py
+++ app.py
@@ -1,7 +1,41 @@
 from flask import Flask, render_template, request, redirect
 from models import Session, Event, create_db
+import requests
+
 
 app = Flask(__name__)
+
+
+api_key = "8cd4a140103c344ce454e7da889d1ced"
+
+
+
+
+def get_weather(city):
+    r = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric")
+    data = r.json()
+    weather_data = {
+        "temp":data["main"]["temp"],
+        "feels_like": data["main"]["feels_like"],
+        "pressure":data["main"]["pressure"]
+
+
+    }
+    p = data["main"]
+    print(p)
+
+    return weather_data
+
+
+
+
+@app.get('/')
+def index():
+    city = request.args.get('city')
+    weather = get_weather(city)
+    return render_template('index.html', weather=weather, city=city)
+
+
 
 
 @app.get('/admadd/')
@@ -24,9 +58,6 @@ def adm():
     return render_template('admin_add.html')
 
 
-@app.get('/')
-def index():
-    return render_template('index.html')
 
 
 @app.get('/menu')
